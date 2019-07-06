769f2071f68514afc70556fd303d7415cc0d93dc
José Castor <junior.castor@gmail.com>
2019-07-06 12:47:48 -0300

    Add style global

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,11 +3,6 @@ import React from 'react';
 import { Title } from './styles';
 
 function Main() {
-  return (
-    <Title error>
-      Main &nbsp;
-      <small>menor</small>
-    </Title>
-  );
+  return <Title>Hello World</Title>;
 }
 export default Main;
